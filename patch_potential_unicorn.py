7fabc3ca1e491a334803c65c68c9b0bec5060ac5
jacksechler <jacksechler@marsk12.org>
2026-09-04 09:49:37 -0400

    Add installer for Unicorn highlighting

---
+++ patch_potential_unicorn.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+p=Path('index.html')
+s=p.read_text()
+needle='<script src="mobile_grid_fix.js?v=3"></script>'
+if needle not in s:
+    needle='<script src="mobile_grid_fix.js?v=2"></script>'
+script='<script src="potential_unicorn.js?v=1"></script>'
+if script not in s:
+    if needle in s:
+        s=s.replace(needle, needle+'\n'+script)
+    else:
+        s=s.replace('</body>', script+'\n</body>')
+p.write_text(s)
