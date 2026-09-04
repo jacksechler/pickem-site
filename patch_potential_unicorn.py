8de07bc343ec6dfdeb65e6072799aa06d181cbfb
jacksechler <jacksechler@marsk12.org>
2026-09-04 09:52:14 -0400

    Cache bust Unicorn grid styling

--- patch_potential_unicorn.py
+++ patch_potential_unicorn.py
@@ -5,8 +5,11 @@ s=p.read_text()
 needle='<script src="mobile_grid_fix.js?v=3"></script>'
 if needle not in s:
     needle='<script src="mobile_grid_fix.js?v=2"></script>'
-script='<script src="potential_unicorn.js?v=1"></script>'
-if script not in s:
+old='<script src="potential_unicorn.js?v=1"></script>'
+script='<script src="potential_unicorn.js?v=2"></script>'
+if old in s:
+    s=s.replace(old,script)
+elif script not in s:
     if needle in s:
         s=s.replace(needle, needle+'\n'+script)
     else:
